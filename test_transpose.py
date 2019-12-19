4df8b83253a3d641d4bc993b3f829b0f2127ec14
Adam Hooper <adam@adamhooper.com>
2019-12-19 18:56:58 -0500

    Implement migrate_params()

--- test_transpose.py
+++ test_transpose.py
@@ -10,17 +10,17 @@ from pandas.testing import assert_frame_equal
 import transpose
 
 
-Column = namedtuple('Column', ('name', 'type'))  # transpose ignores 'format'
+Column = namedtuple("Column", ("name", "type"))  # transpose ignores 'format'
 
 
-def render(table, firstcolname='', input_columns=None):
+def render(table, firstcolname="", input_columns=None):
     def _infer_type(series):
         if is_numeric_dtype(series):
-            return 'number'
+            return "number"
         elif is_datetime64_dtype(series):
-            return 'datetime'
+            return "datetime"
         else:
-            return 'text'
+            return "text"
 
     def _infer_column(colname):
         return Column(colname, _infer_type(table[colname]))
@@ -29,13 +29,22 @@ def render(table, firstcolname='', input_columns=None):
         input_columns = {c: _infer_column(c) for c in table.columns}
 
     return transpose.render(
-        table,
-        {'firstcolname': firstcolname},
-        input_columns=input_columns
+        table, {"firstcolname": firstcolname}, input_columns=input_columns
     )
 
 
-class TransposeTest(unittest.TestCase):
+class MigrateParamsTest(unittest.TestCase):
+    def test_v0(self):
+        self.assertEqual(transpose.migrate_params({}), {"firstcolname": ""})
+
+    def test_v1(self):
+        # as of 2019-02-11
+        self.assertEqual(
+            transpose.migrate_params({"firstcolname": "A"}), {"firstcolname": "A"}
+        )
+
+
+class RenderTest(unittest.TestCase):
     def test_normal(self):
         #     A  B  C
         #  0  b  c  d
@@ -46,34 +55,22 @@ class TransposeTest(unittest.TestCase):
         #          A  b  c
         #  0       B  c  d
         #  1       C  d  e
-        table = pd.DataFrame({
-            'A': ['b', 'c'],
-            'B': ['c', 'd'],
-            'C': ['d', 'e'],
-        })
+        table = pd.DataFrame({"A": ["b", "c"], "B": ["c", "d"], "C": ["d", "e"]})
         result = render(table)
 
-        assert_frame_equal(result, pd.DataFrame({
-            'A': ['B', 'C'],
-            'b': ['c', 'd'],
-            'c': ['d', 'e'],
-        }))
+        assert_frame_equal(
+            result, pd.DataFrame({"A": ["B", "C"], "b": ["c", "d"], "c": ["d", "e"]})
+        )
 
     def test_rename_first_column(self):
         # As above, but with a user supplied first column name
 
-        table = pd.DataFrame({
-            'A': ['b', 'c'],
-            'B': ['c', 'd'],
-            'C': ['d', 'e'],
-        })
-        result = render(table, 'Fish')
+        table = pd.DataFrame({"A": ["b", "c"], "B": ["c", "d"], "C": ["d", "e"]})
+        result = render(table, "Fish")
 
-        assert_frame_equal(result, pd.DataFrame({
-            'Fish': ['B', 'C'],
-            'b': ['c', 'd'],
-            'c': ['d', 'e'],
-        }))
+        assert_frame_equal(
+            result, pd.DataFrame({"Fish": ["B", "C"], "b": ["c", "d"], "c": ["d", "e"]})
+        )
 
     def test_empty_input(self):
         table = pd.DataFrame()
@@ -81,9 +78,9 @@ class TransposeTest(unittest.TestCase):
         assert_frame_equal(result, pd.DataFrame())
 
     def test_empty_input_with_columns(self):
-        table = pd.DataFrame({'A': [], 'B': []}, dtype=object)
+        table = pd.DataFrame({"A": [], "B": []}, dtype=object)
         result = render(table)
-        assert_frame_equal(result, pd.DataFrame({'A': ['B']}))
+        assert_frame_equal(result, pd.DataFrame({"A": ["B"]}))
 
     def test_colnames_to_str(self):
         #     A   B  C
@@ -99,20 +96,27 @@ class TransposeTest(unittest.TestCase):
         #  1       C  d  e   f        g
 
         dt = datetime.datetime(2018, 10, 16, 12, 7)
-        table = pd.DataFrame({
-            'A': [1.1, 2.2, 3.3, None],
-            'B': ['c', 'd', 'e', 'f'],
-            'C': ['d', 'e', 'f', 'g'],
-        })
+        table = pd.DataFrame(
+            {
+                "A": [1.1, 2.2, 3.3, None],
+                "B": ["c", "d", "e", "f"],
+                "C": ["d", "e", "f", "g"],
+            }
+        )
         result = render(table)
 
-        assert_frame_equal(result['dataframe'], pd.DataFrame({
-            'A': ['B', 'C'],
-            '1.1': ['c', 'd'],
-            '2.2': ['d', 'e'],
-            '3.3': ['e', 'f'],
-            'unnamed': ['f', 'g'],
-        }))
+        assert_frame_equal(
+            result["dataframe"],
+            pd.DataFrame(
+                {
+                    "A": ["B", "C"],
+                    "1.1": ["c", "d"],
+                    "2.2": ["d", "e"],
+                    "3.3": ["e", "f"],
+                    "unnamed": ["f", "g"],
+                }
+            ),
+        )
 
     def test_warn_and_rename_on_duplicates(self):
         #     A  B  C
@@ -124,88 +128,108 @@ class TransposeTest(unittest.TestCase):
         #     Column  b  b
         #  0       B  c  d
         #  1       C  d  e
-        table = pd.DataFrame({
-            'A': ['b', 'b'],
-            'B': ['c', 'd'],
-            'C': ['d', 'e'],
-        })
+        table = pd.DataFrame({"A": ["b", "b"], "B": ["c", "d"], "C": ["d", "e"]})
         result = render(table)
 
-        self.assertEqual(result[1], (
-            'We renamed some columns because the input column "A" had '
-            'duplicate values.'
-        ))
-        assert_frame_equal(result[0], pd.DataFrame({
-            'A': ['B', 'C'],
-            'b': ['c', 'd'],
-            'b 1': ['d', 'e'],
-        }))
+        self.assertEqual(
+            result[1],
+            (
+                'We renamed some columns because the input column "A" had '
+                "duplicate values."
+            ),
+        )
+        assert_frame_equal(
+            result[0],
+            pd.DataFrame({"A": ["B", "C"], "b": ["c", "d"], "b 1": ["d", "e"]}),
+        )
 
     def test_warn_and_rename_on_empty_and_unnamed_colname(self):
-        table = pd.DataFrame({'A': ['x', '', 'unnamed', np.nan],
-                              'B': ['b1', 'b2', 'b3', 'b4']})
+        table = pd.DataFrame(
+            {"A": ["x", "", "unnamed", np.nan], "B": ["b1", "b2", "b3", "b4"]}
+        )
         result = render(table)
-        self.assertEqual(result[1], (
-            'We renamed some columns because the input column "A" had '
-            'empty values.'
-        ))
-        assert_frame_equal(result[0], pd.DataFrame({
-            'A': ['B'],
-            'x': ['b1'],
-            'unnamed 1': ['b2'],
-            'unnamed': ['b3'],
-            'unnamed 2': ['b4'],
-        }))
+        self.assertEqual(
+            result[1],
+            (
+                'We renamed some columns because the input column "A" had '
+                "empty values."
+            ),
+        )
+        assert_frame_equal(
+            result[0],
+            pd.DataFrame(
+                {
+                    "A": ["B"],
+                    "x": ["b1"],
+                    "unnamed 1": ["b2"],
+                    "unnamed": ["b3"],
+                    "unnamed 2": ["b4"],
+                }
+            ),
+        )
 
     def test_warn_on_convert_to_str_including_column_header(self):
-        table = pd.DataFrame({'A': [1, 2], 'B': ['x', 'y'], 'C': [3, 4]})
+        table = pd.DataFrame({"A": [1, 2], "B": ["x", "y"], "C": [3, 4]})
         result = render(table)
         assert_frame_equal(
-            result['dataframe'],
-            pd.DataFrame({'A': ['B', 'C'], '1': ['x', '3'], '2': ['y', '4']})
+            result["dataframe"],
+            pd.DataFrame({"A": ["B", "C"], "1": ["x", "3"], "2": ["y", "4"]}),
         )
-        self.assertEqual(result['error'], (
-            'Headers in column "A" were auto-converted to text.\n'
-            'Column "C" was auto-converted to Text because all columns must '
-            'have the same type.'
-        ))
-        self.assertEqual(result['quick_fixes'], [{
-            'text': 'Convert "A", "C" to text',
-            'action': 'prependModule',
-            'args': ['converttotext', {'colnames': ['A', 'C']}],
-        }])
+        self.assertEqual(
+            result["error"],
+            (
+                'Headers in column "A" were auto-converted to text.\n'
+                'Column "C" was auto-converted to Text because all columns must '
+                "have the same type."
+            ),
+        )
+        self.assertEqual(
+            result["quick_fixes"],
+            [
+                {
+                    "text": 'Convert "A", "C" to text',
+                    "action": "prependModule",
+                    "args": ["converttotext", {"colnames": ["A", "C"]}],
+                }
+            ],
+        )
 
     def test_allow_max_n_columns(self):
-        table = pd.DataFrame({
-            'A': pd.Series([chr(x + 100)
-                            for x in range(transpose.MAX_N_COLUMNS)]),
-            'B': pd.Series([chr(x + 120)
-                            for x in range(transpose.MAX_N_COLUMNS)]),
-        })
+        table = pd.DataFrame(
+            {
+                "A": pd.Series([chr(x + 100) for x in range(transpose.MAX_N_COLUMNS)]),
+                "B": pd.Series([chr(x + 120) for x in range(transpose.MAX_N_COLUMNS)]),
+            }
+        )
         result = render(table)
 
         # Build expected result as a dictionary first
-        d = {'A': ['B']}
+        d = {"A": ["B"]}
         for i in range(0, transpose.MAX_N_COLUMNS):
             d[chr(i + 100)] = chr(i + 120)
 
         assert_frame_equal(result, pd.DataFrame(d))
 
     def test_truncate_past_max_n_columns(self):
-        table = pd.DataFrame({
-            'A': pd.Series([str(x)
-                            for x in range(transpose.MAX_N_COLUMNS + 1)]),
-            'B': pd.Series([str(x + 1000)
-                            for x in range(transpose.MAX_N_COLUMNS + 1)]),
-        })
+        table = pd.DataFrame(
+            {
+                "A": pd.Series([str(x) for x in range(transpose.MAX_N_COLUMNS + 1)]),
+                "B": pd.Series(
+                    [str(x + 1000) for x in range(transpose.MAX_N_COLUMNS + 1)]
+                ),
+            }
+        )
         result = render(table)
 
-        self.assertEqual(result[1], (
-            f'We truncated the input to {transpose.MAX_N_COLUMNS} rows so the '
-            'transposed table would have a reasonable number of columns.'
-        ))
+        self.assertEqual(
+            result[1],
+            (
+                f"We truncated the input to {transpose.MAX_N_COLUMNS} rows so the "
+                "transposed table would have a reasonable number of columns."
+            ),
+        )
         # Build expected result as a dictionary first
-        d = {'A': ['B']}
+        d = {"A": ["B"]}
         for i in range(transpose.MAX_N_COLUMNS):
             d[str(i)] = str(i + 1000)
 
@@ -218,33 +242,31 @@ class TransposeTest(unittest.TestCase):
         # Akin to https://github.com/pandas-dev/pandas/issues/19136
         #
         # Column names should strings, not a CategoricalIndex.
-        table = pd.DataFrame({
-            'A': pd.Series(['a1', 'a2'], dtype='category'),  # becomes ret.columns
-            'B': pd.Series(['b1', 'b2']),
-        })
-        result = render(table, firstcolname='X')
-        assert_frame_equal(result, pd.DataFrame({
-            'X': ['B'],
-            'a1': ['b1'],
-            'a2': ['b2'],
-        }))
+        table = pd.DataFrame(
+            {
+                "A": pd.Series(["a1", "a2"], dtype="category"),  # becomes ret.columns
+                "B": pd.Series(["b1", "b2"]),
+            }
+        )
+        result = render(table, firstcolname="X")
+        assert_frame_equal(
+            result, pd.DataFrame({"X": ["B"], "a1": ["b1"], "a2": ["b2"]})
+        )
 
     def test_warn_and_rename_column_if_firstcolname_conflicts(self):
-        table = pd.DataFrame({
-            'X': ['B', 'C'],
-            'A': ['c', 'd'],
-        })
-        result = render(table, firstcolname='B')
-        self.assertEqual(result[1], (
-            'We renamed some columns because the input column "X" had '
-            'duplicate values.'
-        ))
-        assert_frame_equal(result[0], pd.DataFrame({
-            'B': ['A'],
-            'B 1': ['c'],
-            'C': ['d'],
-        }))
+        table = pd.DataFrame({"X": ["B", "C"], "A": ["c", "d"]})
+        result = render(table, firstcolname="B")
+        self.assertEqual(
+            result[1],
+            (
+                'We renamed some columns because the input column "X" had '
+                "duplicate values."
+            ),
+        )
+        assert_frame_equal(
+            result[0], pd.DataFrame({"B": ["A"], "B 1": ["c"], "C": ["d"]})
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- transpose.py
+++ transpose.py
@@ -6,8 +6,9 @@ from typing import Iterator, Set
 MAX_N_COLUMNS = 99
 
 
-def _uniquize_colnames(colnames: Iterator[str],
-                       never_rename_to: Set[str]) -> Iterator[str]:
+def _uniquize_colnames(
+    colnames: Iterator[str], never_rename_to: Set[str]
+) -> Iterator[str]:
     """
     Rename columns to prevent duplicates or empty column names.
 
@@ -20,15 +21,12 @@ def _uniquize_colnames(colnames: Iterator[str],
     for colname in colnames:
         force_add_number = False
         if not colname:
-            colname = 'unnamed'
-            force_add_number = 'unnamed' in never_rename_to
+            colname = "unnamed"
+            force_add_number = "unnamed" in never_rename_to
         if colname in seen or force_add_number:
             for i in range(1, 999999):
-                try_colname = f'{colname} {i}'
-                if (
-                    try_colname not in seen
-                    and try_colname not in never_rename_to
-                ):
+                try_colname = f"{colname} {i}"
+                if try_colname not in seen and try_colname not in never_rename_to:
                     colname = try_colname
                     break
 
@@ -43,8 +41,8 @@ def render(table, params, *, input_columns):
     if len(table) > MAX_N_COLUMNS:
         table = table.truncate(after=MAX_N_COLUMNS - 1)
         warnings.append(
-            f'We truncated the input to {MAX_N_COLUMNS} rows so the '
-            'transposed table would have a reasonable number of columns.'
+            f"We truncated the input to {MAX_N_COLUMNS} rows so the "
+            "transposed table would have a reasonable number of columns."
         )
 
     if not len(table.columns):
@@ -53,14 +51,14 @@ def render(table, params, *, input_columns):
 
     # If user does not supply a name (default), use the input table's first
     # column name as the output table's first column name.
-    first_colname = params['firstcolname'].strip() or table.columns[0]
+    first_colname = params["firstcolname"].strip() or table.columns[0]
 
     column = table.columns[0]
     headers_series = table[column]
     table.drop(column, axis=1, inplace=True)
 
     # Ensure headers are string. (They will become column names.)
-    if input_columns[column].type != 'text':
+    if input_columns[column].type != "text":
         warnings.append(f'Headers in column "A" were auto-converted to text.')
         colnames_auto_converted_to_text.append(column)
 
@@ -78,8 +76,8 @@ def render(table, params, *, input_columns):
     # catch that later.
     na = headers_series.isna()
     headers_series = headers_series.astype(str)
-    headers_series[na] = ''  # Empty values are all equivalent
-    headers_series[headers_series.isna()] = ''
+    headers_series[na] = ""  # Empty values are all equivalent
+    headers_series[headers_series.isna()] = ""
     headers = headers_series.tolist()
     headers.insert(0, first_colname)
     non_empty_headers = [h for h in headers if h]
@@ -88,38 +86,35 @@ def render(table, params, *, input_columns):
     # rename any duplicate/empty headers to.
     unique_headers = set(headers)
 
-    if '' in unique_headers:
+    if "" in unique_headers:
         warnings.append(
             f'We renamed some columns because the input column "{column}" had '
-            'empty values.'
+            "empty values."
         )
-    if len(non_empty_headers) != len(unique_headers - set([''])):
+    if len(non_empty_headers) != len(unique_headers - set([""])):
         warnings.append(
             f'We renamed some columns because the input column "{column}" had '
-            'duplicate values.'
+            "duplicate values."
         )
 
     headers = list(_uniquize_colnames(headers, unique_headers))
 
     table.index = headers[1:]
 
-    input_types = set(c.type
-                      for c in input_columns.values()
-                      if c.name != column)
+    input_types = set(c.type for c in input_columns.values() if c.name != column)
     if len(input_types) > 1:
         # Convert everything to text before converting. (All values must have
         # the same type.)
-        to_convert = [c for c in table.columns
-                      if input_columns[c].type != 'text']
+        to_convert = [c for c in table.columns if input_columns[c].type != "text"]
         colnames_auto_converted_to_text.extend(to_convert)
         if len(to_convert) == 1:
             start = f'Column "{to_convert[0]}" was'
         else:
-            colnames = ', '.join(f'"{c}"' for c in to_convert)
-            start = f'Columns {colnames} were'
+            colnames = ", ".join(f'"{c}"' for c in to_convert)
+            start = f"Columns {colnames} were"
         warnings.append(
-            f'{start} auto-converted to Text because all columns must have '
-            'the same type.'
+            f"{start} auto-converted to Text because all columns must have "
+            "the same type."
         )
 
         for colname in to_convert:
@@ -136,20 +131,32 @@ def render(table, params, *, input_columns):
     ret.reset_index(inplace=True)
 
     if warnings and colnames_auto_converted_to_text:
-        colnames = ', '.join(f'"{c}"' for c in colnames_auto_converted_to_text)
+        colnames = ", ".join(f'"{c}"' for c in colnames_auto_converted_to_text)
         return {
-            'dataframe': ret,
-            'error': '\n'.join(warnings),
-            'quick_fixes': [{
-                'text': f'Convert {colnames} to text',
-                'action': 'prependModule',
-                'args': [
-                    'converttotext',
-                    {'colnames': colnames_auto_converted_to_text},
-                ],
-            }]
+            "dataframe": ret,
+            "error": "\n".join(warnings),
+            "quick_fixes": [
+                {
+                    "text": f"Convert {colnames} to text",
+                    "action": "prependModule",
+                    "args": [
+                        "converttotext",
+                        {"colnames": colnames_auto_converted_to_text},
+                    ],
+                }
+            ],
         }
     if warnings:
-        return (ret, '\n'.join(warnings))
+        return (ret, "\n".join(warnings))
     else:
         return ret
+
+
+def _migrate_params_v0_to_v1(params):
+    return {"firstcolname": ""}
+
+
+def migrate_params(params):
+    if "firstcolname" not in params:
+        params = _migrate_params_v0_to_v1(params)
+    return params
